9eecc38d36b4b060e26bda5a04dcc096226b4482
Simone Westphal <sw@simonewestphal.de>
2020-12-26 10:24:31 +0100

    init: Initial configuration and starter files

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React  from "react";
+
+
+/* class based component, because it holds different states of application */
+
+class App extends React.Component {
+    constructor (props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>App</div>
+        )
+    }
+}
+
+export default App;
